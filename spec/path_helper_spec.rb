5d646a53e6015e482ecda018926a40ec987879ee
Bernd Ahlers <bernd@tuneafish.de>
2011-10-18 22:13:38 +0200

    Add specs for all path helpers.

--- spec/path_helper_spec.rb
+++ spec/path_helper_spec.rb
@@ -10,6 +10,60 @@ describe "PathHelper" do
 
   let(:helper) { PathTest.new }
 
+  describe "path helper methods" do
+    [ ['prefix', '/usr'],
+      ['etc', '/etc'],
+      ['var', '/var'],
+      ['bin', '/usr/bin'],
+      ['doc', '/usr/share/doc'],
+      ['include', '/usr/include'],
+      ['info', '/usr/share/info'],
+      ['lib', '/usr/lib'],
+      ['libexec', '/usr/libexec'],
+      ['man', '/usr/share/man'],
+      ['man1', '/usr/share/man/man1'],
+      ['man2', '/usr/share/man/man2'],
+      ['man3', '/usr/share/man/man3'],
+      ['man4', '/usr/share/man/man4'],
+      ['man5', '/usr/share/man/man5'],
+      ['man6', '/usr/share/man/man6'],
+      ['man7', '/usr/share/man/man7'],
+      ['man8', '/usr/share/man/man8'],
+      ['sbin', '/usr/sbin'],
+      ['share', '/usr/share'] ].each do |m|
+
+      name, path = m
+
+      describe "##{name}" do
+        context "without an argument" do
+          it "returns #{path}" do
+            helper.send(name).to_s.must_equal path
+          end
+        end
+
+        context "with an argument" do
+          it "adds the argument to the path" do
+            helper.send(name, 'foo/bar').to_s.must_equal "#{path}/foo/bar"
+          end
+        end
+
+        context "with a nil argument" do
+          it "does not add anything to the path" do
+            helper.send(name, nil).to_s.must_equal path
+          end
+        end
+
+        context "with installing set to true" do
+          before { helper.installing = true}
+
+          it "adds the destdir as prefix" do
+            helper.send(name, 'blah').to_s.must_equal "#{helper.destdir}#{path}/blah"
+          end
+        end
+      end
+    end
+  end
+
   describe "#installing?" do
     context "with installing set to true" do
       before { helper.installing = true}
