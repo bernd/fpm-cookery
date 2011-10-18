e884883b2265743813b237ce0e2c4f948e813d79
Bernd Ahlers <bernd@tuneafish.de>
2011-10-18 23:22:33 +0200

    Add fixture_path helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
+def fixture_path(file)
+  File.expand_path("../fixtures/#{file}", __FILE__)
+end
+
 # For my rspec poisoned brain. ;)
 module Kernel
   alias_method :context, :describe
