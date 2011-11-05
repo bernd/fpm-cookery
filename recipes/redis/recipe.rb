efde958738fccfea78cc62148c4571354ba97c79
Bernd Ahlers <bernd@tuneafish.de>
2011-11-05 12:32:40 +0100

    Add git source example.

--- recipes/redis/recipe.rb
+++ recipes/redis/recipe.rb
@@ -1,8 +1,15 @@
 class Redis < FPM::Cookery::Recipe
   homepage 'http://redis.io'
-  # different source methods
+
+  # Different source methods.
+  #
   #source   'https://github.com/antirez/redis/trunk', :with => :svn
   #source   'https://github.com/antirez/redis/trunk', :with => :svn, :revision => '2400'
+  #
+  #source   'https://github.com/antirez/redis', :with => :git, :tag => '2.4.2
+  #source   'https://github.com/antirez/redis', :with => :git, :branch => '2.4'
+  #source   'https://github.com/antirez/redis', :with => :git, :sha => '072a905'
+
   source    'http://redis.googlecode.com/files/redis-2.4.2.tar.gz'
   md5      'c4b0b5e4953a11a503cb54cf6b09670e'
 
