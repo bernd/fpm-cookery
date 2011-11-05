aa7bbb81d8ed3259b6a6e9a950adf36f6098e948
Bernd Ahlers <bernd@tuneafish.de>
2011-11-05 12:29:07 +0100

    Start a git handler.

---
+++ lib/fpm/cookery/source_handler/git.rb
@@ -0,0 +1,55 @@
+require 'fpm/cookery/source_handler/template'
+
+module FPM
+  module Cookery
+    class SourceHandler
+      class Git < FPM::Cookery::SourceHandler::Template
+        CHECKSUM = false
+        NAME = :git
+
+        def fetch
+          if local_path.exist?
+            Dir.chdir(local_path) do
+              git('fetch', url)
+            end
+          else
+            Dir.chdir(cachedir) do
+              git('clone', url, local_path)
+            end
+          end
+
+          local_path
+        end
+
+        def extract
+          (builddir/local_path.basename.to_s).mkpath
+
+          extracted_source = (builddir/local_path.basename.to_s).to_s
+
+          Dir.chdir(local_path) do
+            if options[:sha]
+              git('reset', '--hard', options[:sha])
+            elsif options[:tag]
+              git('checkout', '-f', options[:tag])
+            elsif options[:branch]
+              git('checkout', '-f', "origin/#{options[:branch]}")
+            else
+              git('checkout', '-f', 'origin/HEAD')
+            end
+
+            # Trailing '/' is important! (see git-checkout-index(1))
+            git('checkout-index', '-a', '-f', "--prefix=#{extracted_source}/")
+          end
+
+          extracted_source
+        end
+
+        private
+        def git(command, *args)
+          #puts "[DEBUG] git #{command} #{args.join(' ')}"
+          safesystem('git', command, *args)
+        end
+      end
+    end
+  end
+end
