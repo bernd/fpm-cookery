03816055af8183336d355a3bc3b03a698b9ee9b6
Bernd Ahlers <bernd@tuneafish.de>
2012-12-12 10:13:50 +0100

    Cleanup.

--- lib/fpm/cookery/source_handler/curl.rb
+++ lib/fpm/cookery/source_handler/curl.rb
@@ -31,11 +31,11 @@ module FPM
             when '.zip'
               safesystem('unzip', '-d', local_path.basename('.zip'), local_path)
             else
-              if ! local_path.basename().exist?
-                Dir.mkdir(local_path.basename())
+              unless local_path.basename.exist?
+                Dir.mkdir(local_path.basename)
               end
 
-              safesystem('cp', "-f", local_path, local_path.basename())
+              safesystem('cp', '-f', local_path, local_path.basename)
             end
             extracted_source
           end
