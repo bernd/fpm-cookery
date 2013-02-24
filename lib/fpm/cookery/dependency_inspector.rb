d4695dad4f8397c4545f6d450fc7459cb15fe36d
Andy Sykes <github@tinycat.co.uk>
2013-02-24 00:24:23 +0000

    Use Puppet to install packages, using the default provider

--- lib/fpm/cookery/dependency_inspector.rb
+++ lib/fpm/cookery/dependency_inspector.rb
@@ -1,14 +1,46 @@
+require 'puppet'
+require 'puppet/resource'
+require 'puppet/transaction/report'
 require 'fpm/cookery/facts'
+require 'fpm/cookery/log'
+
+# Init Puppet before using it
+Puppet.initialize_settings
 
 module FPM
   module Cookery
     class DependencyInspector
-      # Inspect dependencies!
-      # FPM::Cookery::Packager calls:
-      # DependencyInspector.verify!(recipe.depends, recipe.build_depends)
-      def verify!(depends, build_depends)
-        # Do some work here
+
+      def self.verify!(depends, build_depends)
+
+        Log.info "Verifying and installing build_depends and depends with Puppet"
+
+        build_depends.each do |package|
+          self.install_package(package)
+        end
+
+        depends.each do |package|
+          self.install_package(package)
+        end
+
       end
+
+      private
+      def self.install_package(package)
+        # Use Puppet to install a package
+        resource = Puppet::Resource.new("package", package, :parameters => {
+          :ensure => "present"
+        })
+        result = Puppet::Resource.indirection.save(resource)[1]
+        if result.status == "failed"
+          Log.fatal "While processing depends package '#{package}':"
+          result.logs.each {|log_line| Log.fatal log_line}
+        else
+          Log.info "Processing depends package '#{package}'"
+          result.logs.each {|log_line| Log.info log_line}
+        end
+      end
+
     end
   end
 end
