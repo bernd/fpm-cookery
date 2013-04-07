b45844c7763e4dc0a322796e16984032f176b491
Bernd Ahlers <bernd@tuneafish.de>
2013-04-07 14:23:36 -0400

    Add omnibus-style recipe for fpm-cookery.
    This creates a package for fpm-cookery including ruby.

---
+++ recipes/fpm-cookery/recipe.rb
@@ -0,0 +1,72 @@
+class FPMCookery < FPM::Cookery::Recipe
+  description 'building packages'
+
+  name     'fpm-cookery'
+  version  '0.13.0'
+  revision 0
+  homepage 'https://github.com/bernd/fpm-cookery'
+
+  source '', :with => :noop
+
+  omnibus_package true
+  omnibus_recipes 'ruby'
+  omnibus_dir     '/opt/fpm-cookery'
+
+  def build
+    gem_install 'fpm-cookery', version
+  end
+
+  def install
+    destdir('bin').install workdir('fpm-cook.bin'), 'fpm-cook'
+
+    with_trueprefix do
+      create_post_install_hook
+      create_pre_uninstall_hook
+    end
+  end
+
+  private
+
+  def gem_install(name, version = nil)
+    v = version.nil? ? '' : "-v #{version}"
+    cleanenv_safesystem "#{destdir}/embedded/bin/gem install #{v} #{name}"
+  end
+
+  def create_post_install_hook
+    File.open(builddir('post-install'), 'w', 0755) do |f|
+      f.write <<-__POSTINST
+#!/bin/sh
+set -e
+
+BIN_PATH="#{destdir}/bin"
+BIN="fpm-cook"
+
+update-alternatives --install /usr/bin/$BIN $BIN $BIN_PATH/$BIN 100
+
+exit 0
+      __POSTINST
+
+      self.class.post_install(File.expand_path(f.path))
+    end
+  end
+
+  def create_pre_uninstall_hook
+    File.open(builddir('pre-uninstall'), 'w', 0755) do |f|
+      f.write <<-__PRERM
+#!/bin/sh
+set -e
+
+BIN_PATH="#{destdir}/bin"
+BIN="fpm-cook"
+
+if [ "$1" != "upgrade" ]; then
+  update-alternatives --remove $BIN $BIN_PATH/$BIN
+fi
+
+exit 0
+        __PRERM
+
+      self.class.pre_uninstall(File.expand_path(f.path))
+    end
+  end
+end
---
+++ recipes/fpm-cookery/ruby.rb
@@ -0,0 +1,32 @@
+class Ruby200 < FPM::Cookery::Recipe
+  description 'The Ruby virtual machine'
+
+  name    'ruby'
+  version '2.0.0.0'
+  revision 0
+  homepage 'http://www.ruby-lang.org/'
+  source   'http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p0.tar.bz2'
+  sha256   'c680d392ccc4901c32067576f5b474ee186def2fcd3fcbfa485739168093295f'
+
+  license 'The Ruby License'
+  section 'interpreters'
+
+  build_depends 'autoconf', 'libreadline6-dev', 'bison', 'zlib1g-dev',
+                'libssl-dev', 'libyaml-dev', 'libffi-dev', 'libgdbm-dev', 'libncurses5-dev',
+                'libreadline6-dev'
+
+  depends 'libffi6', 'libncurses5', 'libreadline6', 'libssl1.0.0',
+          'libtinfo5', 'libyaml-0-2', 'zlib1g', 'libffi6', 'libgdbm3', 'libncurses5',
+          'libreadline6'
+
+  def build
+    configure :prefix => destdir, 'disable-install-doc' => true
+    make
+  end
+
+  def install
+    make :install
+
+    rm_f "#{destdir}/lib/libruby-static.a"
+  end
+end
