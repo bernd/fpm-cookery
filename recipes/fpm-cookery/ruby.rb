6e7c187e11bde917cbd29f4ae4f16af5c7757b2f
Jan Ivar Beddari <jan.ivar.beddari@uib.no>
2013-07-01 21:48:03 +0200

    Beginning support rpm/RHEL-variants in the fpm-cookery recipe

--- recipes/fpm-cookery/ruby.rb
+++ recipes/fpm-cookery/ruby.rb
@@ -11,13 +11,23 @@ class Ruby200 < FPM::Cookery::Recipe
   license 'The Ruby License'
   section 'interpreters'
 
-  build_depends 'autoconf', 'libreadline6-dev', 'bison', 'zlib1g-dev',
-                'libssl-dev', 'libyaml-dev', 'libffi-dev', 'libgdbm-dev',
-                'libncurses5-dev', 'libreadline6-dev'
+  platforms [:ubuntu, :debian] do
+    build_depends 'autoconf', 'libreadline6-dev', 'bison', 'zlib1g-dev',
+                  'libssl-dev', 'libyaml-dev', 'libffi-dev', 'libgdbm-dev',
+                  'libncurses5-dev', 'libreadline6-dev'
+    depends 'libffi6', 'libncurses5', 'libreadline6', 'libssl1.0.0',
+            'libtinfo5', 'libyaml-0-2', 'zlib1g', 'libffi6', 'libgdbm3',
+            'libncurses5', 'libreadline6'
+  end
 
-  depends 'libffi6', 'libncurses5', 'libreadline6', 'libssl1.0.0',
-          'libtinfo5', 'libyaml-0-2', 'zlib1g', 'libffi6', 'libgdbm3',
-          'libncurses5', 'libreadline6'
+  platforms [:fedora, :redhat, :centos] do
+    build_depends 'rpmdevtools', 'libffi-devel', 'autoconf', 'bison',
+                  'libxml2-devel', 'libxslt-devel', 'openssl-devel',
+                  'gdbm-devel', 'zlib-devel'
+    depends 'zlib', 'libffi', 'gdbm'
+  end
+  platforms [:fedora] do depends.push('openssl-libs') end
+  platforms [:redhat, :centos] do depends.push('openssl') end
 
   def build
     configure :prefix => destdir,
