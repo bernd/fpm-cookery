f2f7fb8d55b2cd88942f1c5422b391a68fb0d63b
Bernd Ahlers <bernd@tuneafish.de>
2013-09-13 17:40:31 +0200

    Add InvalidConfigKey error.

--- lib/fpm/cookery/exceptions.rb
+++ lib/fpm/cookery/exceptions.rb
@@ -2,6 +2,10 @@ module FPM
   module Cookery
     class Error < StandardError
       MethodNotImplemented = Class.new(self)
+
+      class InvalidConfigKey < self
+        attr_accessor :invalid_keys
+      end
     end
   end
 end
