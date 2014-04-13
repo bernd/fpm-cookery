619c27236dab944985fd00bf5f54e9bf757fef82
Henrik <henrik@haf.se>
2014-04-13 16:41:10 +0200

    testing just yielding builddir

--- lib/fpm/cookery/source_handler/curl.rb
+++ lib/fpm/cookery/source_handler/curl.rb
@@ -48,7 +48,7 @@ module FPM
           cmd << (config[:quiet] ? '-s' : '--progress-bar')
           cmd += ['-o', path, url]
           safesystem(*cmd)
-        rescue RuntimeError => e
+        rescue RuntimeError
           Log.error("Command failed: #{cmd.join(' ')}")
           raise
         end
@@ -68,14 +68,7 @@ module FPM
           when 1
             entries.first
           else
-            ext = Path.new(url).extname
-            dir = local_path.basename(ext)
-
-            if File.exist?(dir)
-              dir
-            else
-              raise "Could not find source directory for #{local_path.basename}"
-            end
+            builddir
           end
         end
       end
