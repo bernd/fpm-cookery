5417a3881ed7568763f5164717f23da9e6aa025e
Bernd Ahlers <bernd@tuneafish.de>
2014-05-29 08:52:56 +0200

    Expose skip-package config option as command line flag.
    See #86.

--- lib/fpm/cookery/cli.rb
+++ lib/fpm/cookery/cli.rb
@@ -27,6 +27,8 @@ module FPM
         :attribute_name => 'pkg_dir'
       option '--cache-dir', 'DIR', 'directory for downloaded sources',
         :attribute_name => 'cache_dir'
+      option '--skip-package', :flag, 'do not call FPM to build the package',
+        :attribute_name => 'skip_package'
 
       class Command < self
         def recipe_file
