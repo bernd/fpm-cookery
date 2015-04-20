82d6567a3e11c708d06078b8256e95601b62577a
Ryan Underwood <runderwood@machinezone.com>
2015-04-20 16:15:05 -0700

    Stop translating underscores in configure args

--- lib/fpm/cookery/utils.rb
+++ lib/fpm/cookery/utils.rb
@@ -24,7 +24,6 @@ module FPM
         if args.last.is_a?(Hash)
           opts = args.pop
           args += opts.map{ |k,v|
-            option = k.to_s.gsub('_','-')
             if v == true
               "--#{option}"
             else
