ac8494e9972afaa6498ef3cc59fd7c41616bdb7e
Brian OConnell <boc@us.ibm.com>
2015-08-27 16:22:53 -0400

    Added support for DirRecipe and Directory Handler

--- lib/fpm/cookery/recipe.rb
+++ lib/fpm/cookery/recipe.rb
@@ -14,6 +14,7 @@ require 'fpm/cookery/package/npm'
 require 'fpm/cookery/package/pear'
 require 'fpm/cookery/package/python'
 require 'fpm/cookery/package/virtualenv'
+require 'fpm/cookery/log'
 
 module FPM
   module Cookery
@@ -230,6 +231,30 @@ module FPM
       attr_rw :virtualenv_pypi, :virtualenv_install_location, :virtualenv_fix_name
       def input(config)
         FPM::Cookery::Package::Virtualenv.new(self, config)
+      end
+    end
+
+    # Helps packaging a directory of content
+    class DirRecipe < Recipe
+
+      def input(config)
+        FPM::Cookery::Package::Dir.new(self, config)
+      end
+
+      # Dir Recipes by default build action.
+      def build
+      end
+
+      # Default action for a dir recipe install is to copy items selected
+      def install
+        FileUtils.cp_r File.join(builddir, '.'), destdir
+        # Remove build cookies
+        %w(build extract).each do |cookie|
+          Dir.glob("#{destdir}/.#{cookie}-cookie-*").each do |f|
+            Log.info "Deleting FPM Cookie #{f}"
+            File.delete(f)
+          end
+        end
       end
     end
   end
--- lib/fpm/cookery/version.rb
+++ lib/fpm/cookery/version.rb
@@ -1,5 +1,5 @@
 module FPM
   module Cookery
-    VERSION = '0.31.0'
+    VERSION = '0.32.0'
   end
 end
