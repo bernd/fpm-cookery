04994852f1f4ad582258ffa8dd8b44b7291627af
David O'Rourke <david@scalefactory.com>
2015-11-30 16:58:49 +0000

    Don't dereference_root during #install.
    This should preserve symlinks during install as cp_r usually
    dereferences them.

--- lib/fpm/cookery/path.rb
+++ lib/fpm/cookery/path.rb
@@ -66,7 +66,7 @@ module FPM
 
         # We used to use :preserve => true here, but that broke package
         # building when the file tree contains broken symlinks.
-        FileUtils.cp_r src, dst
+        FileUtils.cp_r src, dst, :dereference_root => false
 
         # if File.symlink? src
         #   # we use the BSD mv command because FileUtils copies the target and
